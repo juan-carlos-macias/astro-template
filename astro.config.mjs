246921b26d6f7e9a4ec95655b8be3a9178c1c195
Juan Carlos Macias <maciasnajeraj@gmail.com>
2025-11-30 01:13:25 -0600

    fix: fix button language error

--- astro.config.mjs
+++ astro.config.mjs
@@ -4,10 +4,10 @@ import { defineConfig } from 'astro/config';
 // https://astro.build/config
 export default defineConfig({
 	i18n: {
-		defaultLocale: 'en',
+		defaultLocale: 'es',
 		locales: ['en', 'es'],
 		routing: {
-			prefixDefaultLocale: true
+			prefixDefaultLocale: false
 		}
 	}
 });
